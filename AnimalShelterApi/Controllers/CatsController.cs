04529995990fb53d04864001eb310c84690a1ccd
osbrown1 <socknot@live.com>
2023-03-31 10:05:09 -0700

    set up Put() controller action

--- AnimalShelterApi/Controllers/CatsController.cs
+++ AnimalShelterApi/Controllers/CatsController.cs
@@ -35,5 +35,49 @@ namespace AnimalShelterApi.Controllers
 
       return cat;
     }
+
+    // POST api/Cats
+    [HttpPost]
+    public async Task<ActionResult<Cat>> Post(Cat cat)
+    {
+      _db.Cats.Add(cat);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetCat), new { id = cat.CatId }, cat);
+    }
+
+    // PUT: api/Cats/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Cat cat)
+    {
+      if (id != cat.CatId)
+      {
+        return BadRequest();
+      }
+
+      _db.Cats.Update(cat);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!CatExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool CatExists(int id)
+    {
+      return _db.Cats.Any(e => e.CatId == id);
+    }
   }
 }
--- AnimalShelterApi/Controllers/DogsController.cs
+++ AnimalShelterApi/Controllers/DogsController.cs
@@ -35,6 +35,49 @@ namespace AnimalShelterApi.Controllers
 
       return dog;
     }
+
+    // POST api/Dogs
+    [HttpPost]
+    public async Task<ActionResult<Dog>> Post(Dog dog)
+    {
+      _db.Dogs.Add(dog);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetDog), new { id = dog.DogId }, dog);
+    }
+
+    // PUT: api/Dogs/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Dog dog)
+    {
+      if (id != dog.DogId)
+      {
+        return BadRequest();
+      }
+
+      _db.Dogs.Update(dog);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!DogExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool DogExists(int id)
+    {
+      return _db.Dogs.Any(e => e.DogId == id);
+    }
   }
 } 
-//
