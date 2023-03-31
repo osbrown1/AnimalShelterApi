19b595e56bf2bb6a341653ed999cffd8063a7973
osbrown1 <socknot@live.com>
2023-03-31 09:17:47 -0700

    Seeded Data

---
+++ AnimalShelterApi/Migrations/20230331161555_SeedData.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, 7, "Siamese", "Jeremy" },
+                    { 2, 4, "Sphynx", "Matilda" },
+                    { 3, 9, "Persian", "Sarah" },
+                    { 4, 3, "Burmese", "Jake" },
+                    { 5, 2, "Siberian", "Bartholomeow" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, 2, "Golden Retriever", "Garfield" },
+                    { 2, 5, "Bulldog", "Bazinga" },
+                    { 3, 8, "Poodle", "Rigby" },
+                    { 4, 1, "German Sheperd", "Clarence" },
+                    { 5, 6, "Border Collie", "Stitch" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -4,11 +4,32 @@ namespace AnimalShelterApi.Models
 {
   public class AnimalShelterApiContext : DbContext
   {
-    public DbSet<Dog> Dogs { get; set; }
     public DbSet<Cat> Cats { get; set; }
+    public DbSet<Dog> Dogs { get; set; }
 
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat { CatId = 1, Name = "Jeremy", Breed = "Siamese", Age = 7 },
+          new Cat { CatId = 2, Name = "Matilda", Breed = "Sphynx", Age = 4 },
+          new Cat { CatId = 3, Name = "Sarah", Breed = "Persian", Age = 9 },
+          new Cat { CatId = 4, Name = "Jake", Breed = "Burmese", Age = 3 },
+          new Cat { CatId = 5, Name = "Bartholomeow", Breed = "Siberian", Age = 2 }
+         );
+
+         builder.Entity<Dog>()
+        .HasData(
+          new Dog { DogId = 1, Name = "Garfield", Breed = "Golden Retriever", Age = 2 },
+          new Dog { DogId = 2, Name = "Bazinga", Breed = "Bulldog", Age = 5 },
+          new Dog { DogId = 3, Name = "Rigby", Breed = "Poodle", Age = 8 },
+          new Dog { DogId = 4, Name = "Clarence", Breed = "German Sheperd", Age = 1 },
+          new Dog { DogId = 5, Name = "Stitch", Breed = "Border Collie", Age = 6 }
+        );
     }
   }
 }
