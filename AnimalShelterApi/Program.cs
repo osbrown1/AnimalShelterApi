a0b59bee6637c442a51d0ba0307cc89de32fc486
osbrown1 <socknot@live.com>
2023-03-31 08:50:24 -0700

    update Program.cs to add MySql to db service, add models.

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,12 +1,24 @@
+using AnimalShelterApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AnimalShelterApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Updated code below!
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
